67cdba53ea09c93faf0100effd4c52193bb5da67
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-24 14:56:04 +0100

    Add project files.

---
+++ Oefening2_WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace Oefening2_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AantalKleuren.Text = "Aantal kleuren = " + typeof(Colors).GetProperties().Length;
+            ComboBoxColors.ItemsSource = typeof(Colors).GetProperties();
+            ListBoxColors.ItemsSource = typeof(Colors).GetProperties();
+            Debug.WriteLine("Aantal elementen in Listbox=" + ListBoxColors.Items.Count);
+        }
+    }
+}
